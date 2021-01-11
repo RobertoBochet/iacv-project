8e1c8d139aadf4834f899d59c1b7063835614246
Roberto Bochet <robertobochet@gmail.com>
2021-01-11 05:52:15 +0100

    Added functions to forge projective matrices

--- roberto/paintar/utilities/functions.py
+++ roberto/paintar/utilities/functions.py
@@ -17,9 +17,9 @@ def proj2cart(x) -> np.array:
     """
     x = proj_normalization(x)
     if np.ndim(x) == 1:
-        return x[:-2]
+        return x[:-1]
     else:
-        return x[:, :-2]
+        return x[:, :-1]
 
 
 def proj_normalization(x) -> np.array:
@@ -30,3 +30,38 @@ def proj_normalization(x) -> np.array:
         return x / x[-1]
     else:
         return (x.T / x[:, -1]).T
+
+
+def forge_isometry(r: np.array, t: np.array) -> np.array:
+    """
+    forges an isometry from rotation and translation matrices
+    """
+    assert r.shape[0] == t.shape[0], "R and T must have the same number of rows"
+    assert r.shape[0] == r.shape[1], "R must be square matrix"
+    assert t.ndim == 1, "T must be a column vector"
+
+    t = t[:, np.newaxis]
+    a = np.vstack((
+        np.hstack((r, t)),
+        np.zeros((1, r.shape[1] + 1))
+    ))
+    a[-1, -1] = 1
+
+    return a
+
+
+def forge_projective_matrix(k: np.array, r: np.array = None, t: np.array = None, a: np.array = None) -> np.array:
+    """
+    forges a projective matrix given camera matrix K, and geometrical transformation (R,T) or A
+    """
+    assert a is not None or (r is not None and t is not None), "(R,T) or A must be provided"
+
+    if a is not None:
+        assert a.shape == (4, 4), "shape of A must be (4,4)"
+        a = a / a[-1, -1]
+        return k @ a[:-1]
+
+    else:
+        assert r.shape == (3, 3), "shape of R must be (3,3)"
+        assert t.shape == (3,), "shape of T must be 3"
+        return k @ np.hstack((r, t[..., np.newaxis]))
