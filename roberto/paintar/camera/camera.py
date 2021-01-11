3f699a32e1ff9d047516a6f57438ce54032917dc
Roberto Bochet <robertobochet@gmail.com>
2021-01-11 05:53:04 +0100

    Added function to estimate pose of the aruco

--- roberto/paintar/camera/camera.py
+++ roberto/paintar/camera/camera.py
@@ -1,7 +1,7 @@
 import cv2.cv2 as cv
 import numpy as np
 
-from ..utilities import Chessboard
+from ..utilities import Chessboard, forge_isometry, forge_projective_matrix
 
 
 class Camera(cv.VideoCapture):
@@ -32,9 +32,22 @@ class Camera(cv.VideoCapture):
         return self._t
 
     @property
-    def p(self) -> np.array:
+    def a(self) -> np.array:
+        """
+        provides the geometrical transformation A_b^c (i.e. p^c = A_b^c p^b),
+        the transformation of the base frame respect of camera frame
+        """
+        assert self._r is not None and self._t is not None, "camera must be calibrated"
+        return forge_isometry(self._r, self._t)
+
+    @property
+    def m(self) -> np.array:
+        """
+        provides the camera matrix M_b^c (i.e. x^c = M_b^c X^b),
+        the transformation from point in P^3 referred to base frame to point in P^2 in camera projection
+        """
         assert self._k is not None and self._r is not None and self._t is not None, "camera must be calibrated"
-        return self._k @ np.hstack((self._r, self._t))
+        return forge_projective_matrix(self._k, r=self._r, t=self._t)
 
     def shot(self) -> np.array:
         _, img = self.read()
@@ -62,9 +75,10 @@ class Camera(cv.VideoCapture):
             np.copyto(debug_buffer, img, casting="no")
             cv.drawChessboardCorners(debug_buffer, chessboard.size, corners, True)
 
-        ret, r, self._t, _ = cv.solvePnPRansac(chessboard.get_points(), corners, self._k, self._dist)
+        ret, r, t, _ = cv.solvePnPRansac(chessboard.get_points(), corners, self._k, self._dist)
 
         self._r, _ = cv.Rodrigues(r)
+        self._t = t.reshape(3)
 
         return ret
 
@@ -74,14 +88,14 @@ class Camera(cv.VideoCapture):
         if grab:
             self.grab()
 
-        img = self.retrieve_undistorted()
+        _, img = self.retrieve()
 
         img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         img = cv.equalizeHist(img)
 
         corners, ids, _ = cv.aruco.detectMarkers(img, aruco_dict, parameters=aruco_param,
                                                  cameraMatrix=self._k,
-                                                 # distCoeff=self._dist
+                                                 distCoeff=self._dist
                                                  )
         if ids is None:
             # no aruco detected
@@ -98,3 +112,26 @@ class Camera(cv.VideoCapture):
             return None
 
         return np.squeeze(aruco[0][1])
+
+    def find_aruco_pose(self, aruco_id: int, marker_size: float, debug_buffer: np.array = None, **kwargs):
+        """
+        provides the geometrical transformation A_a^c (i.e. p^c = A_a^c p^a),
+        the transformation of the aruco frame respect of camera frame
+        """
+        aruco = self.find_aruco(aruco_id, **kwargs)
+
+        if aruco is None:
+            return None
+
+        r, t, _ = cv.aruco.estimatePoseSingleMarkers([aruco], marker_size, self._k, self._dist)
+
+        if debug_buffer is not None:
+            _, img = self.retrieve()
+            debug_buffer.resize(img.shape, refcheck=False)
+            np.copyto(debug_buffer, img, casting="unsafe")
+            cv.aruco.drawAxis(debug_buffer, self._k, self._dist, r, t, 2 * marker_size)
+
+        r, _ = cv.Rodrigues(r[0])
+        t = t.reshape(3)
+
+        return forge_isometry(r, t)
