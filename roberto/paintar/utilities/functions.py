9682cefd4fb86594bd9973dd3a150b4b77d0867e
Roberto Bochet <robertobochet@gmail.com>
2021-01-12 03:30:00 +0100

    Fixed several bug in utilities

--- roberto/paintar/utilities/functions.py
+++ roberto/paintar/utilities/functions.py
@@ -7,6 +7,8 @@ def cart2proj(x) -> np.array:
     """
     if np.ndim(x) == 1:
         return np.hstack((x, [1]))
+    elif np.ndim(x) == 2 and x.shape[1] == 1:
+        return np.vstack((x, [1]))
     else:
         return np.hstack((x, np.ones((x.shape[0], 1))))
 
@@ -16,7 +18,7 @@ def proj2cart(x) -> np.array:
     converts one or several points from projective P^2/P^3 to cartesian 2D/3D
     """
     x = proj_normalization(x)
-    if np.ndim(x) == 1:
+    if np.ndim(x) == 1 or (np.ndim(x) == 2 and x.shape[1] == 1):
         return x[:-1]
     else:
         return x[:, :-1]
@@ -26,7 +28,7 @@ def proj_normalization(x) -> np.array:
     """
     returns the normalized version of a vector or list of vectors in the projective space
     """
-    if np.ndim(x) == 1:
+    if np.ndim(x) == 1 or (np.ndim(x) == 2 and x.shape[1] == 1):
         return x / x[-1]
     else:
         return (x.T / x[:, -1]).T
@@ -54,7 +56,7 @@ def forge_projective_matrix(k: np.array, r: np.array = None, t: np.array = None,
     """
     forges a projective matrix given camera matrix K, and geometrical transformation (R,T) or A
     """
-    assert k.shape == (3,3), "k must be square matrix (3x3)"
+    assert k.shape == (3, 3), "k must be square matrix (3x3)"
 
     if a is not None:
         assert a.shape == (4, 4), "shape of A must be (4,4)"
@@ -67,4 +69,4 @@ def forge_projective_matrix(k: np.array, r: np.array = None, t: np.array = None,
         return k @ np.hstack((r, t[..., np.newaxis]))
 
     else:
-        return np.hstack((k, np.zeros((3,1))))
+        return np.hstack((k, np.zeros((3, 1))))
