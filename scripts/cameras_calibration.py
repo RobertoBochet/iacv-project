b543d3154102136439a844e24072ee7f9cf6d9fb
Roberto Bochet <robertobochet@gmail.com>
2021-02-05 04:09:37 +0100

    Added script to calibrate cameras

---
+++ scripts/cameras_calibration.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+import numpy as np
+import cv2 as cv
+
+from paintar.camera import Camera
+from paintar.utilities import Chessboard
+
+PATH_IMAGES = Path("./images/chessboard")
+PATH_DATA = Path("./data")
+
+PATH_IMAGES_CAMERA_1 = PATH_IMAGES / "camera1"
+PATH_IMAGES_CAMERA_2 = PATH_IMAGES / "camera2"
+
+PATH_DATA_CAMERA_1 = PATH_DATA / "camera1"
+PATH_DATA_CAMERA_2 = PATH_DATA / "camera2"
+
+PATH_INTRINSICS_CAMERA_1 = PATH_DATA_CAMERA_1 / "intrinsics.txt"
+PATH_INTRINSICS_CAMERA_2 = PATH_DATA_CAMERA_2 / "intrinsics.txt"
+
+PATH_DISTORTION_CAMERA_1 = PATH_DATA_CAMERA_1 / "distortion.txt"
+PATH_DISTORTION_CAMERA_2 = PATH_DATA_CAMERA_2 / "distortion.txt"
+
+CHESSBOARD_SIZE = (9, 6)
+CHESSBOARD_SQUARE_SIZE = 0.0245
+
+if __name__ == "__main__":
+    chessboard = Chessboard(CHESSBOARD_SIZE, CHESSBOARD_SQUARE_SIZE)
+
+    cam1 = Camera()
+    cam2 = Camera()
+
+    path_images_1 = PATH_IMAGES_CAMERA_1.glob("*.jpg")
+    path_images_2 = PATH_IMAGES_CAMERA_2.glob("*.jpg")
+
+    images_1 = np.array([cv.imread(str(path), cv.IMREAD_GRAYSCALE) for path in path_images_1])
+    images_2 = np.array([cv.imread(str(path), cv.IMREAD_GRAYSCALE) for path in path_images_2])
+
+    cam1.calibrate(images_1, chessboard)
+    cam2.calibrate(images_2, chessboard)
+
+    np.savetxt(PATH_INTRINSICS_CAMERA_1, cam1.k)
+    np.savetxt(PATH_INTRINSICS_CAMERA_2, cam2.k)
+    np.savetxt(PATH_DISTORTION_CAMERA_1, cam1.dist)
+    np.savetxt(PATH_DISTORTION_CAMERA_2, cam2.dist)
