b96ce4ad6f4d2848cf9dbbaa7570f59c50a52551
Roberto Bochet <robertobochet@gmail.com>
2021-02-05 23:25:22 +0100

    Minor code changes

--- paintar/__main__.py
+++ paintar/__main__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import time
 from pathlib import Path
 
 from paintar.syncer import synchronize, estimate_delay
@@ -7,18 +6,14 @@ from ._cv import cv
 import numpy as np
 
 from paintar.canvas import Canvas
-from . import utilities as ut
 from .camera import Camera, StereoCamera
 
-PATH_VIDEO = Path("./video")
-PATH_VIDEO_1 = PATH_VIDEO / "video1_2.avi"
-PATH_VIDEO_2 = PATH_VIDEO / "video2_2.avi"
-PATH_VIDEO_AR = PATH_VIDEO / "ar.mp4"
+PATH_VIDEO = Path("./data/video")
+PATH_PARAMETERS = Path("./data/parameters")
 
-SKIP_FRAMES = 30 * 10 * 0
-TEST_FRAMES = 1500
+PATH_VIDEO_1 = PATH_VIDEO / "video1.avi"
+PATH_VIDEO_2 = PATH_VIDEO / "video2.avi"
 
-PATH_PARAMETERS = Path("./data")
 PATH_PARAMETERS_1 = PATH_PARAMETERS / "camera1"
 PATH_PARAMETERS_2 = PATH_PARAMETERS / "camera2"
 
@@ -28,11 +23,11 @@ DIST1 = np.loadtxt(PATH_PARAMETERS_1 / "distortion.txt")
 DIST2 = np.loadtxt(PATH_PARAMETERS_2 / "distortion.txt")
 R1 = np.loadtxt(PATH_PARAMETERS_1 / "R.txt")
 R2 = np.loadtxt(PATH_PARAMETERS_2 / "R.txt")
-T1 = np.loadtxt(PATH_PARAMETERS_1 / "t.txt") / 1000
-T2 = np.loadtxt(PATH_PARAMETERS_2 / "t.txt") / 1000
+T1 = np.loadtxt(PATH_PARAMETERS_1 / "t.txt")
+T2 = np.loadtxt(PATH_PARAMETERS_2 / "t.txt")
 
-CHESSBOARD_SIZE = (9, 6)
-CHESSBOARD_SQUARE_SIZE = 0.0245
+SKIP_FRAMES = 30 * 10 * 0
+TEST_FRAMES = 1500
 
 ARUCO_PEN_TIP_OFFSET = np.array([0, -0.139, 0, 1])
 ARUCO_PEN_ID = 0
@@ -51,8 +46,6 @@ T_CANVAS = np.block([
 ])
 
 if __name__ == "__main__":
-    chessboard = ut.Chessboard(CHESSBOARD_SIZE, CHESSBOARD_SQUARE_SIZE)
-
     cam1 = Camera(PATH_VIDEO_1.as_posix(), k=K1, dist=DIST1, r=R1, t=T1)
     cam2 = Camera(PATH_VIDEO_2.as_posix(), k=K2, dist=DIST2, r=R2, t=T2)
 
@@ -61,7 +54,7 @@ if __name__ == "__main__":
     # to test de-sync
     # for _ in range(40):
     #    cam1.grab()
-    if False:
+    if True:
         dt = estimate_delay(stereo_cam,
                             aruco_dict=ARUCO_DICT,
                             aruco_id=ARUCO_PEN_ID,
@@ -94,14 +87,10 @@ if __name__ == "__main__":
                     plot=True,
                     project=True)
 
-    print(canvas.size_meters)
-
     for _ in range(SKIP_FRAMES):
         stereo_cam.grab()
 
     while True:
-        t_i = time.time()
-
         if not stereo_cam.grab():
             break
 
